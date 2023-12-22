f495f73f87840d9b48e3d3edc65312f7bdb13808
njeriwachira <87851470+Njeriee@users.noreply.github.com>
2023-12-22 17:26:48 +0000

    code updated

--- changeName.py
+++ changeName.py
@@ -3,7 +3,12 @@
 import sys
 import subprocess
 
-with open 'oldFiles.txt' as f:
-    old_substring = 
-    new_substring = 'jdoe'
-    string.replace(old_substring, new_substring)
+file = open(sys.argv[1])
+lines = file.readlines()
+print(lines)
+for line in lines:
+    line.strip()
+    new_line = line.replace('jane','jdoe')
+    print(new_line)
+    subprocess.run(['mv','line','new_line'])
+file.close()
